747de875f39d0d8d52f66f2e9ff7b461b3b63fea
Brian Adams <brian.lives.outdoors@gmail.com>
2017-11-08 12:54:35 -0500

    add counters, change formatting

--- src/common.ts
+++ src/common.ts
@@ -1,24 +1,24 @@
 import { MongoClient } from 'mongodb';
 
 export const connection = (closure) => {
-    return MongoClient.connect('mongodb://localhost:27017/mean', (err, db) => {
-        if (err)  { return console.log(err); }
-        closure(db);
-    });
+  return MongoClient.connect('mongodb://localhost:27017/mean', (err, db) => {
+    if (err)  { return console.log(err); }
+    closure(db);
+  });
 };
 
 export const createSuccess = (data) => {
-    return {
-        status: 200,
-        data: data,
-        message: null
-    };
+  return {
+    status: 200,
+    data: data,
+    message: null
+  };
 };
 
 export const createError = (err) => {
-    return {
-        status: 501,
-        data: null,
-        message: typeof err === 'object' ? err.message : err
-    };
+  return {
+    status: 501,
+    data: null,
+    message: typeof err === 'object' ? err.message : err
+  };
 };
---
+++ src/routes/counters.ts
@@ -0,0 +1,48 @@
+import {Router, Request, Response, NextFunction} from 'express';
+import {ObjectId} from 'mongodb';
+import {connection, createSuccess, createError} from '../common';
+
+class CountersRouter {
+  router: Router;
+
+  constructor() {
+    this.router = Router();
+    this.init();
+  }
+
+  private get(req: Request, res: Response) {
+    connection((db) => {
+      db.collection('counters')
+        .findOne()
+        .then((counters) => {
+          res.json(createSuccess(counters));
+        })
+        .catch((err) => {
+          res.status(501).json(createError(err));
+        });
+    });
+  }
+
+  private post(req: Request, res: Response, next: NextFunction): void {
+    connection((db) => {
+      db.collection('counters')
+        .findOneAndUpdate({}, req.body,
+        {
+          upsert: true
+        })
+        .then((result) => {
+          res.json(createSuccess(result))
+        })
+        .catch((err) => {
+          res.status(501).json(createError(err));
+        });
+    });
+  }
+
+  private init(): void {
+    this.router.get('', this.get);
+    this.router.post('', this.post);
+  }
+}
+
+export default new CountersRouter().router;
